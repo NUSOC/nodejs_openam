cc8ab3b41816282db7dc151dd4c783886e3b9c77
richard <r@no.email>
2018-07-20 15:11:36 -0500

    working

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # nodejs_openam
+
+
+Create a file in to hold values. Something like `./config/openam.js`. Fill it with 
+stuff like this
+
+```
+module.exports = {
+    server: 'https://your.open.am.server:443/amserver?goto=',
+    return: 'http://your.url:3000',
+    validate: 'https://your.open.am.server:443/amserver/identity/isTokenValid?tokenid=',
+    details: 'https://your.open.am.server:443/amserver/identity/attributes?subjectid='
+} 
+
+```
+
+
+In App.js, set up 
+```
+// module and config
+var soc_openam = require('./soc_openam');
+var openamConfig = require('./config/openam');
+
+// middle ware for soc_openam
+app.use((req, res, next) => {
+  soc_openam.validate(req.cookies.openAMssoToken)
+    .then(result => {
+        console.log('DEGUG: True');
+        next();
+    })
+    .catch(err => {
+      console.log('DEBUG: false')
+      res.redirect(openamConfig.server + openamConfig.return);
+    });
+    
+});
+```
--- openam.js
+++ openam.js
@@ -5,35 +5,66 @@ const openamCongif = require('../config/openam');
 
 module.exports = {
 
-    // validate token 
-    validate: (token) => {
-        return new Promise((resolve, reject) => {
 
-            // reject if no cookie
-            if (token === undefined) {
-                reject('undefined');
-            }
 
-            if (token === '') {
-                reject('no value');
-            }
 
-            const validate_url = openamCongif.validate + token;
-            fetch(validate_url)
-                .then(result => result.text())
-                .then(body => {
-                    console.log({
-                        body: body,
-                        cookie: token,
-                        validate_url: validate_url
-                    });
+    // Validate token through a Promise. Rejection includes token not 
+    // existing or being blank. 
+    validate: (token) => new Promise((resolve, reject) => {
 
-                    if (body.indexOf('true') >= 0) {
-                        resolve(true)
-                    } else {
-                        reject({token: token});
-                    }
+        // reject if no cookie
+        if (token === undefined) {
+            reject('undefined');
+        }
+
+        if (token === '') {
+            reject('no value');
+        }
+
+        // hit endpoint to validate token
+        const validate_url = openamCongif.validate + token;
+        fetch(validate_url)
+            .then(result => result.text())
+            .then(body => {
+                console.log({
+                    body: body,
+                    cookie: token,
+                    validate_url: validate_url
                 });
-        });
-    }
+
+                if (body.indexOf('true') >= 0) {
+                    console.log('token valid')
+                    resolve(true)
+                } else {
+                    console.log('token invalid')
+                    reject(false);
+                }
+            });
+    }),
+
+
+    //Get Details 
+    getDetails: (token) => new Promise((resolve, reject) => {
+        fetch(openamCongif.details + token)
+            .then(result => result.text())
+            .then(body => {
+
+                const bodyParts = body.split("\n");
+                let bodyObject={};                
+                
+                let index = 1;
+                while (index < bodyParts.length) {
+                    let key = bodyParts[index].split('=')[1]
+                    let val = bodyParts[index + 1].split('=')[1]
+                    bodyObject[key]=val
+                    index++;
+                    index++;                    
+                }
+                
+                resolve(bodyObject);
+            })
+            .catch((err)=>reject(err));
+    })
+
+
 }
